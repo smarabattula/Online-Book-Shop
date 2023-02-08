f473abcb6e552fc68b1f1a9b43f7b4718ba509f8
sylee <sylee@gmail.com>
2023-02-08 15:26:52 -0500

    Implements hashed passwords

---
+++ db/migrate/20230208202201_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :name
+      t.string :email
+      t.string :address
+      t.string :credit_card_number
+      t.string :phone_number
+      t.boolean :is_admin
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
