a589e4efc7f08531429d9a33ffa551f336fd1e5c
Sukruthi Modem <smodem@ncsu.edu>
2023-02-11 14:20:57 -0600

    added book associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :transactions
+  has_many :books
+  has_many :reviews
   validates :username, presence: true, uniqueness: true
-
 end
