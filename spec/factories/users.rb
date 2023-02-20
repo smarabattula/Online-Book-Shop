d4e5900cdb53c35aad088991948cada3bcd7fd50
seungyoung Lee <slee82@ncsu.edu>
2023-02-19 21:08:37 -0500

    fix bug on session_test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
   factory :user do
     sequence(:username) { |n| "user#{n}" }
-    password {"1"}
+    password {"123456"}
     sequence(:name) { |n| "user#{n}" }
     email {"aaa@ncsu.com"}
     phone_number {"1231231231"}
+    credit_card_number {"1231231231231231"}
+    address {"aaaaaaaaa"}
   end
   end
