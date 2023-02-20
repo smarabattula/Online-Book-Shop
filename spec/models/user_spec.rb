d937be693df711a7b1fcba5e054b130414c4848d
seungyoung Lee <slee82@ncsu.edu>
2023-02-19 20:40:06 -0500

    Include add validations on user model + modify db.seed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe User, type: :model do
     subject { FactoryBot.build(:user) }
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username)}
-  end
+    it { should validate_presence_of(:email) }
+    it { should allow_value('example@google.com').for(:email) }
+    it { should_not allow_value('not email format').for(:email) }
+    it { should validate_presence_of(:credit_card_number) }
+    it { should validate_length_of(:credit_card_number).is_at_least(16) }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:phone_number) }
+    it { should validate_length_of(:phone_number).is_at_least(10) }
+    end
 
   describe 'associations' do
     it { should have_many(:transactions).dependent(:destroy) }
